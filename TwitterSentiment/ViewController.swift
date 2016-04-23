fa5843c2b25f57492cc239d23668e5decb96f3c3
Kieran Hall <kieranh5511@gmail.com>
2016-04-23 18:26:46 +0100

    Initial Commit

---
+++ TwitterSentiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterSentiment
+//
+//  Created by Kieran Hall on 23/04/2016.
+//  Copyright © 2016 HackSussex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
